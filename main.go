fcd9f7a885bd2abec9f938281ec5ecf3b5b1d51c
Bin Wang <bin.wang@mail.binwang.me>
2025-04-16 22:22:16 -0400

    host swagger json

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"flag"
 	"net/http"
+	"os"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
@@ -20,31 +21,51 @@ var (
 	grpcServerEndpoint = flag.String("grpc-server-endpoint", "grpc.rssbrain.com:443", "gRPC server endpoint")
 )
 
+func serveSwagger(mux *http.ServeMux) {
+	swaggerFile, err := os.ReadFile("gen/go/grpc-api.swagger.json")
+	if err != nil {
+		grpclog.Fatal(err)
+	}
+
+	mux.HandleFunc("/swagger.json", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(swaggerFile)
+	})
+}
+
 func run() error {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
+	// Create a new ServeMux for HTTP handlers
+	httpMux := http.NewServeMux()
+
 	// Register gRPC server endpoint
 	// Note: Make sure the gRPC server is running properly and accessible
-	mux := runtime.NewServeMux()
-	// opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	grpcGatewayMux := runtime.NewServeMux()
 	creds := credentials.NewClientTLSFromCert(nil, "") // 'nil' means use system roots
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(creds)}
 	err := errors.Join(
-		gw.RegisterArticleAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts),
-		gw.RegisterSourceAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts),
-		gw.RegisterFolderAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts),
-		gw.RegisterUserAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts),
-		gw.RegisterMoreLikeThisAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts),
-		gw.RegisterSystemAPIHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts),
+		gw.RegisterArticleAPIHandlerFromEndpoint(ctx, grpcGatewayMux, *grpcServerEndpoint, opts),
+		gw.RegisterSourceAPIHandlerFromEndpoint(ctx, grpcGatewayMux, *grpcServerEndpoint, opts),
+		gw.RegisterFolderAPIHandlerFromEndpoint(ctx, grpcGatewayMux, *grpcServerEndpoint, opts),
+		gw.RegisterUserAPIHandlerFromEndpoint(ctx, grpcGatewayMux, *grpcServerEndpoint, opts),
+		gw.RegisterMoreLikeThisAPIHandlerFromEndpoint(ctx, grpcGatewayMux, *grpcServerEndpoint, opts),
+		gw.RegisterSystemAPIHandlerFromEndpoint(ctx, grpcGatewayMux, *grpcServerEndpoint, opts),
 	)
 	if err != nil {
 		return err
 	}
 
+	// Serve swagger JSON file
+	serveSwagger(httpMux)
+
+	// Handle gRPC-Gateway endpoints
+	httpMux.Handle("/", grpcGatewayMux)
+
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
-	return http.ListenAndServe(":8881", mux)
+	return http.ListenAndServe(":8881", httpMux)
 }
 
 func main() {
